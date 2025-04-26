e057f7e53028e9899eb6c5809167ff2a18c00dc3
Jeremie Alcaraz <hello@jeremiealcaraz.com>
2025-04-26 09:33:06 +0200

    initial commit

---
+++ src/server/api/routers/post.ts
@@ -0,0 +1,45 @@
+import { z } from "zod";
+
+import {
+	createTRPCRouter,
+	protectedProcedure,
+	publicProcedure,
+} from "~/server/api/trpc";
+import { posts } from "~/server/db/schema";
+
+export const postRouter = createTRPCRouter({
+	hello: publicProcedure
+		.input(z.object({ text: z.string() }))
+		.query(({ input }) => {
+			return {
+				greeting: `Hello ${input.text}`,
+			};
+		}),
+
+		create: protectedProcedure
+		.input(z.object({ name: z.string().min(1) }))
+		.mutation(async ({ ctx, input }) => {
+		  try {
+			await ctx.db.insert(posts).values({
+			  name: input.name,
+			  createdById: ctx.session.user.id,
+			});
+			console.log("Post created successfully");
+		  } catch (error) {
+			console.error("Error creating post:", error);
+			throw error; // Rethrow so client can see there was an error
+		  }
+		}),
+
+	getLatest: protectedProcedure.query(async ({ ctx }) => {
+		const post = await ctx.db.query.posts.findFirst({
+			orderBy: (posts, { desc }) => [desc(posts.createdAt)],
+		});
+
+		return post ?? null;
+	}),
+
+	getSecretMessage: protectedProcedure.query(() => {
+		return "you can now see this secret message!";
+	}),
+});
